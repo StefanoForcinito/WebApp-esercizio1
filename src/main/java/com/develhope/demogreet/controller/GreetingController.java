28c42219ac204e65a647eb24b36a12aac62432bb
StefanoForcinito <stegallo1990@gmail.com>
2024-03-05 18:35:09 +0100

    primo esercizio corretto

--- src/main/java/com/develhope/demogreet/controller/GreetingController.java
+++ src/main/java/com/develhope/demogreet/controller/GreetingController.java
@@ -1,14 +1,16 @@
 package com.develhope.demogreet.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1")
 public class GreetingController {
-    @GetMapping("/v1/ciao")
-    public String greet(@RequestParam(name = "Giuseppe")String nome,
-        @RequestParam(name = "provincia") String provincia) {
+    @GetMapping("/ciao")
+    public String greet(@RequestParam String nome,
+        @RequestParam String provincia) {
         return "Ciao " + nome + ", com'è il tempo in " + provincia + "?";
     }
 }
